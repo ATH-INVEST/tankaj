4c53acce54a795127d4594baa237255009831bb8
Gasper Parte <34384865+gparty@users.noreply.github.com>
2026-04-27 15:16:34 +0200

    Improve mobile navigation and remove install prompt

--- app/layout.tsx
+++ app/layout.tsx
@@ -106,7 +106,6 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col bg-[#06140f] text-white">
         {children}
-        <InstallAppPrompt />
         <MobileAppBar />
         
 
